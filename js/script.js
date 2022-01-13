e6261ca5dd4e01e906ca5b24c8cf9e7180803eac
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2022-01-13 11:32:24 +0100

    The product page has a carousel with two images edited by Photoshop.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-var productLink = document.querySelector(".kitchen");
-var productLinkButton = document.querySelector("#kitchen");
+var productLinkButton = document.querySelectorAll("#kitchen");
 
+console.log(productLinkButton);
 
-productLink.addEventListener("click", goToProductPage);
-productLinkButton.addEventListener("click", goToProductPage);
+productLinkButton.forEach(productLinkButton => productLinkButton.addEventListener("click", goToProductPage));
 
 function goToProductPage() {
     location.href = "/products.html";
